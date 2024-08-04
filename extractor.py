586569ddb9ce0b25690e48b0824a85197ae6d9b3
Banati Balazs <balazs.banati@gmail.com>
2024-08-04 11:21:08 +0200

    bugfix median color of the whole image not the cropped one

--- extractor.py
+++ extractor.py
@@ -34,7 +34,7 @@ def analyze(image, lang):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply thresholding to the image
     _, thresholded_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    # cv2.imshow('thresholded', thresholded_image)
+    cv2.imshow('thresholded', thresholded_image)
     # config = r'--oem 3 --psm 6'
     config = r'--oem 3 --psm 6 -c preserve_interword_spaces=1'
     try:
@@ -61,7 +61,7 @@ def crop_masked_image(image, mask):
     cropped_mask = mask[y:y+h, x:x+w]
 
     # most common color
-    color = np.median(cropped_im, axis=(0, 1))
+    color = np.median(image, axis=(0, 1))
 
     inverted_mask = cv2.bitwise_not(cropped_mask)
     # fill mask with most common color
